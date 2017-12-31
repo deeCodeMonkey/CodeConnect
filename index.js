f6bee48e1daf0fe10228dc2e1b125d73383239df
deeCodeMonkey <deidra108@gmail.com>
2017-12-30 22:09:12 -0700

    test deploy to heroku with configs

--- index.js
+++ index.js
@@ -53,8 +53,17 @@ require('./routes/authRoutes')(app);
 require('./routes/appRoutes')(app);
 
 
+//Heroku
+if (process.env.NODE_ENV === 'production') {
+    //Express will serve up production assets
+    app.use(express.static('client/build'));
+    //Express will serve up index.html if route unknown
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 
-
+}
 
 
 var PORT = process.env.PORT || 8080;
