c13c2fd9fa0c7c11cbd92ec7709e6d242827bdc9
deeCodeMonkey <deidra108@gmail.com>
2018-01-03 15:57:26 -0700

    user profile load complete

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,15 +1,12 @@
 ﻿import { SUBMIT_PROFILE, FETCH_PROFILE } from '../actions/types';
 
 export default function (state = {}, action) {
-    console.log('profile reducer======', action);
+    console.log('profile reducer: action recieved:======', action);
     switch (action.type) {
         case SUBMIT_PROFILE:
-            return {
-                ...state,
-                profileData: action.payload
-            }
+            return action.payload.data
         case FETCH_PROFILE:
-            return action.payload; 
+            return action.payload.data 
         default:
             return state;
     }
