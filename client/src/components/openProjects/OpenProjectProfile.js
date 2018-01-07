c0c5164914784dafcaa6ac8030c9f5fb09c91648
deeCodeMonkey <deidra108@gmail.com>
2018-01-07 09:05:58 -0700

    modal for open projects

---
+++ client/src/components/openProjects/OpenProjectProfile.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+const OpenProjectProfile = (props) => {
+
+    return (
+        <div>
+            <h5>PROJECT PROFILE</h5>
+            <h6>PROJECTID{props.projectId}</h6>
+            <h6>{props.title}</h6>
+            <h6>{props.dueDate}</h6>
+            <h6>{props.requirements}</h6>
+            <h6>{props.projectOwner}</h6>
+            <h6>{props.image}</h6>
+            <h6>{props.description}</h6>
+            <h6>{props.location}</h6>
+            <button>Add Project to User</button>
+        </div>
+    );
+};
+
+
+export default OpenProjectProfile;
